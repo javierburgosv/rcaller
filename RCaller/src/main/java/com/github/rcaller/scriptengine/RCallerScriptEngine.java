379bfc42ca69b622f63d4f941e2f737fbbc54ab7
javierburgosv <j.b.valdes@hotmail.com>
2021-11-10 06:55:52 +0000

    Delete Temp Files from Script Engine

--- RCaller/src/main/java/com/github/rcaller/scriptengine/RCallerScriptEngine.java
+++ RCaller/src/main/java/com/github/rcaller/scriptengine/RCallerScriptEngine.java
@@ -37,7 +37,13 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 
-public class RCallerScriptEngine implements ScriptEngine, EventHandler, Invocable {
+
+public class RCallerScriptEngine
+        implements
+        ScriptEngine,
+        EventHandler,
+        Invocable
+{
 
     final private RCaller rcaller;
     private final RCode rcode;
@@ -45,29 +51,34 @@ public class RCallerScriptEngine implements ScriptEngine, EventHandler, Invocabl
     private Bindings bindings;
     private ScriptContext context;
 
-    public RCallerScriptEngine() {
+
+    public RCallerScriptEngine()
+    {
         rcaller = RCaller.create();
         rcode = RCode.create();
 
         rcode.addRCode("result <- list(a=0)");
         rcaller.setRCode(rcode);
         rcaller.runAndReturnResultOnline("result");
-        
+
         context = new javax.script.SimpleScriptContext();
     }
 
     @Override
-    public Object eval(String code, ScriptContext sc) throws ScriptException {
+    public Object eval(String code, ScriptContext sc) throws ScriptException
+    {
         return (this.eval(code));
     }
 
     @Override
-    public Object eval(Reader reader, ScriptContext sc) throws ScriptException {
+    public Object eval(Reader reader, ScriptContext sc) throws ScriptException
+    {
         return (this.eval(reader));
     }
 
     @Override
-    public Object eval(String code) throws ScriptException {
+    public Object eval(String code) throws ScriptException
+    {
         rcode.clearOnline();
         rcode.addRCode(code);
         rcode.addRCode("result <- list(a=0)");
@@ -77,37 +88,46 @@ public class RCallerScriptEngine implements ScriptEngine, EventHandler, Invocabl
     }
 
     @Override
-    public Object eval(Reader reader) throws ScriptException {
+    public Object eval(Reader reader) throws ScriptException
+    {
         BufferedReader bufferedReader = new BufferedReader(reader);
         String line;
         StringBuilder stringBuilder = new StringBuilder();
 
-        try {
-            while (true) {
+        try
+        {
+            while (true)
+            {
                 line = bufferedReader.readLine();
-                if (line == null) {
+                if (line == null)
+                {
                     break;
                 }
                 stringBuilder.append(line);
             }
-        } catch (IOException ioe) {
+        }
+        catch (IOException ioe)
+        {
             throw new ScriptException("Error while reading from reader: " + ioe.toString());
         }
         return (this.eval(stringBuilder.toString()));
     }
 
     @Override
-    public Object eval(String code, Bindings bndngs) throws ScriptException {
+    public Object eval(String code, Bindings bndngs) throws ScriptException
+    {
         return (this.eval(code));
     }
 
     @Override
-    public Object eval(Reader reader, Bindings bindings) throws ScriptException {
+    public Object eval(Reader reader, Bindings bindings) throws ScriptException
+    {
         return (this.eval(reader));
     }
 
     @Override
-    public void put(String name, Object o) {
+    public void put(String name, Object o)
+    {
         rcode.clearOnline();
         StringBuilder code = new StringBuilder();
         RCodeUtils.addRespectToType(code, name, o, false);
@@ -119,119 +139,155 @@ public class RCallerScriptEngine implements ScriptEngine, EventHandler, Invocabl
     }
 
     @Override
-    public Object get(String var) {
+    public Object get(String var)
+    {
         int[] dimension;
         rcode.clearOnline();
         rcode.addRCode("result <- ls()");
         rcaller.runAndReturnResultOnline(var);
         parser = rcaller.getParser();
         parser.parse();
-        try {
-            //System.out.println(parser.getXMLFileAsString());
+        try
+        {
+            // System.out.println(parser.getXMLFileAsString());
             dimension = parser.getDimensions(var);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             return (parser.getAsStringArray(var));
         }
         String vartype = parser.getType(var);
-        if (dimension[0] > 1 && dimension[1] > 1) {
+        if (dimension[0] > 1 && dimension[1] > 1)
+        {
             return (parser.getAsDoubleMatrix(var));
-        } else if (vartype.equals("numeric")) {
+        }
+        else if (vartype.equals("numeric"))
+        {
             return (parser.getAsDoubleArray(var));
-        } else if (vartype.equals("character")) {
+        }
+        else if (vartype.equals("character"))
+        {
             return (parser.getAsStringArray(var));
-        } else {
+        }
+        else
+        {
             return (parser.getAsStringArray(var)); // :o
         }
     }
 
     @Override
-    public Bindings getBindings(int i) {
+    public Bindings getBindings(int i)
+    {
         return (this.bindings);
     }
 
     @Override
-    public void setBindings(Bindings bndngs, int i) {
+    public void setBindings(Bindings bndngs, int i)
+    {
         this.bindings = bndngs;
     }
 
     @Override
-    public Bindings createBindings() {
+    public Bindings createBindings()
+    {
         return (new SimpleBindings());
     }
 
     @Override
-    public ScriptContext getContext() {
+    public ScriptContext getContext()
+    {
         return (this.context);
     }
 
     @Override
-    public void setContext(ScriptContext sc) {
+    public void setContext(ScriptContext sc)
+    {
         this.context = sc;
     }
 
     @Override
-    public ScriptEngineFactory getFactory() {
+    public ScriptEngineFactory getFactory()
+    {
         return (new RCallerScriptEngineFactory());
     }
 
     @Override
-    public void messageReceived(String senderName, String msg) {
+    public void messageReceived(String senderName, String msg)
+    {
         System.out.println("RCaller Script Engine Received: (" + senderName + ") " + msg);
     }
 
-    public void close() {
+    public void close()
+    {
         this.rcaller.stopRCallerOnline();
     }
 
     /*
-      Methods for invokable interface
+     * Methods for invokable interface
      */
     @Override
-    public Object invokeMethod(Object o, String fname, Object... arguments) throws ScriptException, NoSuchMethodException {
+    public Object invokeMethod(Object o, String fname, Object... arguments)
+            throws ScriptException, NoSuchMethodException
+    {
         return (invokeFunction(fname, arguments));
     }
 
     @Override
-    public Object invokeFunction(String fname, Object... arguments) throws ScriptException, NoSuchMethodException {
+    public Object invokeFunction(String fname, Object... arguments) throws ScriptException, NoSuchMethodException
+    {
         int[] dimension = null;
         String var = "fresult";
 
         rcode.clearOnline();
         rcode.addRCode(var + " <- " + fname + "(");
-        for (int i = 0; i < arguments.length; i++) {
+        for (int i = 0; i < arguments.length; i++)
+        {
             NamedArgument named = (NamedArgument) arguments[i];
             RCodeUtils.addRespectToType(rcode.getCode(), named.getName(), named.getObj(), true);
-            if (i != (arguments.length - 1)) {
+            if (i != (arguments.length - 1))
+            {
                 rcode.addRCode(",");
             }
         }
         rcode.addRCode(")");
         rcaller.setRCode(rcode);
-        //Please do not delete, I like to see verbose output in development stage.
-        //System.out.println(rcode.getCode().toString()); 
+        // Please do not delete, I like to see verbose output in development
+        // stage.
+        // System.out.println(rcode.getCode().toString());
         rcaller.runAndReturnResultOnline(var);
-       
+
         parser = rcaller.getParser();
-        
+
         ArrayList<NamedArgument> namedResults = new ArrayList<>();
         ArrayList<String> names = rcaller.getParser().getNames();
-        
-        for (String name : names) {
+
+        for (String name : names)
+        {
             var = name;
-            try {
+            try
+            {
                 dimension = parser.getDimensions(var);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 namedResults.add(NamedArgument.Named(var, parser.getAsStringArray(var)));
             }
             String vartype = parser.getType(var);
             assert dimension != null;
-            if (dimension[0] > 1 && dimension[1] > 1) {
+            if (dimension[0] > 1 && dimension[1] > 1)
+            {
                 namedResults.add(NamedArgument.Named(var, parser.getAsDoubleMatrix(var)));
-            } else if (vartype.equals("numeric")) {
+            }
+            else if (vartype.equals("numeric"))
+            {
                 namedResults.add(NamedArgument.Named(var, parser.getAsDoubleArray(var)));
-            } else if (vartype.equals("character")) {
+            }
+            else if (vartype.equals("character"))
+            {
                 namedResults.add(NamedArgument.Named(var, parser.getAsStringArray(var)));
-            } else {
+            }
+            else
+            {
                 namedResults.add(NamedArgument.Named(var, parser.getAsStringArray(var)));
             }
         }
@@ -239,16 +295,24 @@ public class RCallerScriptEngine implements ScriptEngine, EventHandler, Invocabl
     }
 
     @Override
-    public <T> T getInterface(Class<T> type) {
+    public <T> T getInterface(Class<T> type)
+    {
         return (null);
     }
 
     @Override
-    public <T> T getInterface(Object o, Class<T> type) {
+    public <T> T getInterface(Object o, Class<T> type)
+    {
         return (null);
     }
 
     /*
-      End of Methods for invokable interface
+     * End of Methods for invokable interface
      */
+
+    // Access to delete temp files functionality
+    public void deleteTempFiles()
+    {
+        this.rcaller.deleteTempFiles();
+    }
 }
